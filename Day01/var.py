34ff8d63aea819c4919dbe0e72d07c846f32483e
ohsangsang <152273187+ohsangsang@users.noreply.github.com>
2023-11-28 21:49:29 +0900

    Add files via upload

---
+++ Day01/var.py
@@ -0,0 +1,26 @@
+#변수(variable)
+#컴퓨터란?
+#폰노이만 구조를 따르면 컴퓨터다.
+#폰노이만 구조 = CPU - RAM -INPUT - OUTPUT
+
+a = 100 # = r-value를 변수에 대입한다 (오른쪽 값을 왼쪽에 대입한다)
+name = "오상은"
+mbti = "enfp"
+
+# 변수 문법
+# 변수명은 문자, 숫자, 밑줄(_) 단, 숫자 시작은 불가. 1name 불가 name1 가능
+# 변수명은 대소문자 구분이 됩니다.
+# 변수 국룰
+# 1. 의미 있는 변수명
+# 2. 소문자로 작성
+# 3. 단어 + 단어: snake 방식, camel 방식 사용
+#    megastudy_python: snake 방식
+#    megastudyPython: camel 방식
+
+newjeans_member = "하니"
+newjeans_song = "ETA"
+
+print(f"내가 좋아하는 뉴진스 멤버는 {newjeans_member}")
+print(f"내가 좋아하는 뉴진스 노래는 {newjeans_song}")
+
+
---
+++ Day01/var_input.py
@@ -0,0 +1,27 @@
+# input은 콘솔에 유저가 입력한 값을 받는 함수
+print("뉴진스 짱짱")
+newjeans_song = input("뉴진스 중에 좋아하는 노래 뭐임?")
+print(f"너가 좋아하는 뉴진스 노래는 {newjeans_song}이구나")
+
+bts_song = input("방탄소년단 노래 중에 좋아하는 노래가 뭐임?")
+print(f"니가 좋아하는 방탄 노래는 {bts_song}이구나")
+
+mbti = input("너 MBTI가 뭐야?")
+print(f"니 MBTI는 {mbti}구나")
+
+first = input("외향입니까? 내향입니까? (E 또는 I 입력)")
+second = input("직관입니까? 감각입니까? (N 또는 S 입력)")
+third = input("감정형입니까? 사고형입니까? (F 또는 P 입력)")
+fourth = input("즉흥적입니까? 계획적입니까? (P 또는 J 입력)")
+print(f"당신의 MBTI는 {first}{second}{third}{fourth}입니다.")
+
+name = input("성함이 어떻게 되십니까?")
+print(f"당신의 이름은 {name}이군요.")
+
+age = input("몇 살이세요?")
+print(f"{age}살이시군요.")
+
+print(f"당신의 MBTI는 {first}{second}{third}{fourth}이고, 이름은 {name}이며, {age}살 이시군요.")
+
+
+
